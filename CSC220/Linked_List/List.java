3f5b705a5e945de3519128d4701149d8b04ba650
Pablo Johnson <paulj.96@gmail.com>
2016-10-05 20:14:13 -0500

    started working on List.java

--- CSC220/Linked_List/List.java
+++ CSC220/Linked_List/List.java
@@ -1,9 +1,10 @@
 /* ***************************************************
- * <your name>
- * <the date>
- * <the file name>
+ * Pablo Johnson
+ * 10-14-16
+ * Linked List
  *
- * <a simple, short program/class description>
+ * Backend for a linked list. 
+ * Since it is an abstract data type, you don't really need to read this :)
  *************************************************** */
 
 // the Node class
@@ -56,23 +57,38 @@ public class List
 	// remember that an empty list has a "size" of -1 and its "position" is at -1
 	public List()
 	{
+		num_items = -1;
+		curr.setData(-1);
+		tail.setLink(head.getLink());
+		head.setLink(tail.getLink());
 	}
 
 	// copy constructor
 	// clones the list l and sets the last element as the current
 	public List(List l)
 	{
+		Node temp_node;
+
+		List new_list = new List;
+		l.SetPos(0);
+		while(l.GetPos() < l.GetSize());	
+			{
+				new_list.InsertAfter(l.GetValue());
+			}
+		return new_list();
 	}
 
 	// navigates to the beginning of the list
 	public void First()
 	{
+		curr = head.getLink();
 	}
 
 	// navigates to the end of the list
 	// the end of the list is at the last valid item in the list
 	public void Last()
 	{
+		curr = tail.getLink();
 	}
 
 	// navigates to the specified element (0-index)
@@ -80,6 +96,14 @@ public class List
 	// this should not be possible for invalid positions
 	public void SetPos(int pos)
 	{
+		if(head.getLink() == null)
+			return;
+		curr = head.getLink()
+		for(int i = 0; i < pos; i++)
+		{
+			curr = curr.getLink();
+		}
+		
 	}
 
 	// navigates to the previous element
@@ -87,6 +111,15 @@ public class List
 	// there should be no wrap-around
 	public void Prev()
 	{
+		int pos_counter = 0;
+		Node last_pos = curr.getLink();
+		curr = head.getLink();
+		while(last_pos.getLink() != curr.getLink().getLink())
+		{
+			pos_counter++;
+			curr = curr.getLink();
+		}
+
 	}
 
 	// navigates to the next element
@@ -94,29 +127,82 @@ public class List
 	// there should be no wrap-around
 	public void Next()
 	{
+		curr = curr.getLink()
 	}
 
 	// returns the location of the current element (or -1)
 	public int GetPos()
 	{
+		if(curr.getData = -1)
+			return curr.getData();
+
+		int pos_counter = 0;
+		Node last_pos = curr.getLink();
+		curr = head.getLink();
+		while(last_pos.getLink() != curr.getLink())
+		{
+			pos_counter ++;
+			curr = curr.getLink();
+		}
+		return pos_counter;
+
 	}
 
 	// returns the value of the current element (or -1)
 	public int GetValue()
 	{
+		return curr.getData();
 	}
 
 	// returns the size of the list
 	// size does not imply capacity
 	public int GetSize()
 	{
+		if(head.getLink() == null)
+			return -1;
+
+		Node temp_node;
+		temp_node.setLink(curr);
+		curr = head.getLink();
+		int counter = 1;
+		while(curr.getLink() != null)
+		{
+			curr = curr.getLink();
+			counter ++;
+		}
+		curr = temp_node;
+		return counter;
 	}
+
 
 	// inserts an item before the current element
 	// the new element becomes the current
 	// this should not be possible for a full list
 	public void InsertBefore(int data)
 	{
+		// Node to hold curr so we can reset it at the end.
+		Node temp_node;
+		temp_node.setLink(curr);
+		
+		// Create new node that we will insert
+		Node new_node = new Node;
+		new_node.setData(data);
+		new_node.setLink(curr.getLink());
+
+		// Set node before new node to point to new node
+		int counter = 0;
+		Node last_pos = curr.getLink();
+		curr = head.getLink();
+		while(last_pos.getLink() != curr.getLink().getLink())
+		{
+			pos_counter ++;
+			curr = curr.getLink();
+		}
+		curr.setLink(new_node);
+		curr = temp_node;
+
+
+
 	}
 
 	// inserts an item after the current element
@@ -124,12 +210,24 @@ public class List
 	// this should not be possible for a full list
 	public void InsertAfter(int data)
 	{
+		if(this.isFull())
+			return;
+
+		Node new_node;
+		new_node.setData(data);
+		new_node.setLink(curr.getLink());
+		curr.setLink(new_node);
+		
 	}
 
 	// removes the current element (collapsing the list)
 	// this should not be possible for an empty list
 	public void Remove()
 	{
+		if(head.getLink() == null)
+			return;
+
+		curr.setData(curr.getLink().getData());
 	}
 
 	// replaces the value of the current element with the specified value
