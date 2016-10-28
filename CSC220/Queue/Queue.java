bb805bfc214123be039ef16e19396be46030fcb2
Pablo Johnson <paulj.96@gmail.com>
2016-10-28 00:09:34 -0500

    commented Queue and Stack

--- CSC220/Queue/Queue.java
+++ CSC220/Queue/Queue.java
@@ -1,60 +1,77 @@
+/***************************************************
+** Pablo Johnson
+** CSC 220
+** Queue
+****************************************************/
+
 public class Queue<QueueType>
 {
+	// use queue as backend
 	private List<QueueType> l;
 
+	// Constructor
 	public Queue()
 	{
 		l = new List<QueueType>();
 	}
 
+	// Clone queue that is passed in
 	public Queue(Queue<QueueType> s)
 	{
 		l = new List<QueueType>(s.l);
 		l.First();
 	}
 
+	// Add to the tail of the queue
 	public void Enqueue(QueueType data)
 	{
 		l.Last();
 		l.InsertAfter(data);
 	}
 
+	// Remove from the front of the queue
 	public QueueType Dequeue()
 	{
 		l.First();
 		QueueType t = Peek();
 
 		l.Remove();
-		
+
 		return t;
 	}
 
+	// See what is at the front / head of queue
 	public QueueType Peek()
 	{
 		l.First();
 		return l.GetValue();
 	}
-	
+
+	// Return size of queue
 	public int Size()
 	{
 		return l.GetSize();
 	}
 
+	// Return if queue is empty
 	public boolean IsEmpty()
 	{
 		return l.IsEmpty();
 	}
 
+	// Return if queue is full
 	public boolean IsFull()
 	{
 		return l.IsFull();
 	}
-	
+
+	// Return if one queue equals another
 	public boolean Equals(Queue s)
 	{
 		return l.Equals(s.l);
 	}
 
+	// Append a queue to another
 	public Queue<QueueType> Add(Queue<QueueType> s)
 	{
 		Queue<QueueType> s2 = new Queue<QueueType>(this);
@@ -66,6 +83,7 @@ public class Queue<QueueType>
 		return s2;
 	}
 
+	// Returns string representation of queue
 	public String toString()
 	{
 		return l.toString();
--- CSC220/Stack/Stack.java
+++ CSC220/Stack/Stack.java
@@ -1,57 +1,75 @@
+/*********************************
+** Pablo Johnson
+** CSC220
+** Stack
+*********************************/
+
+
 public class Stack<StackType>
 {
+	// Use Generic list as backend
 	private List<StackType> l;
 
+	// Constructor
 	public Stack()
 	{
 		l = new List<StackType>();
 	}
 
+	// Clones stack to another stack
 	public Stack(Stack<StackType> s)
 	{
 		l = new List<StackType>(s.l);
 		l.First();
 	}
 
+	// Pushes data onto stack
 	public void Push(StackType data)
 	{
 		l.InsertBefore(data);
 	}
 
+	// Removes data from top of stack 
 	public StackType Pop()
 	{
 		StackType t = Peek();
 
 		l.Remove();
-		
+
 		return t;
 	}
 
+	// See what is on the top of the stack
 	public StackType Peek()
 	{
 		return l.GetValue();
 	}
-	
+
+	// Return height of stack
 	public int Size()
 	{
 		return l.GetSize();
 	}
 
+	// Return true if stack is empty
 	public boolean IsEmpty()
 	{
 		return l.IsEmpty();
 	}
 
+	// Return true if stack is full
 	public boolean IsFull()
 	{
 		return l.IsFull();
 	}
-	
+
+	// Return true if stacks are equal
 	public boolean Equals(Stack s)
 	{
 		return l.Equals(s.l);
 	}
 
+	// Creates new stack from two stacks
 	public Stack<StackType> Add(Stack<StackType> s)
 	{
 		Stack<StackType> s2 = new Stack<StackType>(this);
@@ -63,6 +81,7 @@ public class Stack<StackType>
 		return s2;
 	}
 
+	// Returns string representation of stack
 	public String toString()
 	{
 		return l.toString();
