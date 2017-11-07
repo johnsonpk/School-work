8fd93244b576eb6f45d183b7c5236fd1c1643bca
Pablo Johnson <paulj.96@gmail.com>
2017-11-06 23:52:31 -0600

    Achtually finished hw 4

--- 325CSC/4Homework/trie.py
+++ 325CSC/4Homework/trie.py
@@ -1,72 +1,110 @@
+################################################
+# Pablo Johnson
+# CSC 325
+# Trie Implementation
+# 11.7.2017
+################################################
+
+
+# Node class for our, well, nodes :)
 class Node(object):
-    def __init__(self, data, prev):
-        self.data = data
+# init the node
+# we want our self.word variable to tell us if this node is the end of a word
+# prev will point to the parent node
+# keys will hold an array of none until assigned a child
+# we use 128 here because I originally expected to have to do all ascii characters
+# i.e. you could add "Hello.!" and "Hello" would not register as being stored.
+    def __init__(self, word, prev):
+        self.word = word
         self.prev = prev
         self.keys = [None] * 128
 
+
+# This is our Trie class, it utilizes the node class we created earlier
+# It ignores all non alpha characters, including when checking for is a word is stored
+#   i.e. if you store "Hi!" only "Hi" will be stored.
+#   likewise, if you check .isStored("Hi!") and "Hi" is stored, it will return True
 class Trie(object):
-    def __init__(self, words_to_add = []):
+    # We will init, setting our root to None and our words to add to either "" or whatever is
+    #   passed in. If text is passed in, we will go ahead and utilize our add function
+    def __init__(self, words_to_add = ""):
         self.root = None
         self.words_to_add = words_to_add
+
         if len(self.words_to_add) > 0:
-            for x in self.words_to_add:
-                self.add(x)
+            self.add(self.words_to_add)
 
+    # Function to check if a word is stored. It does differentiate between subwords and words.
+    #   i.e. if "Piper" is stored, "Pipe" will not return as being stored since it was not 
+    #   explicitly stored
     def isStored(self, text):
+        for i in range(len(text)):
+            if not ((65 <= ord(text[i]) <= 90) or (97 <= ord(text[i]) <= 122)):
+                text = text[:i] + " " + text[i+1:]
         node = self.root
-        for letter in text:
-            if node.keys[ord(letter)] is not None:
-                node = node.keys[ord(letter)]
-            else:
-                return False
-
-        return True
+        for i in range(len(text)):
+            if node.keys[ord(text[i])] is not None:
+                node = node.keys[ord(text[i])]
+            # Will only return true if the last letter indicates that it is the end of a word that has
+            #   been explicitly stored
+            if ((i == (len(text) - 1)) and (node.word)):
+                return True
+        # If we reach the end of the word but it hasn't been explicitly stored, return False
+        return False
         
-    def add(self, pattern):
-        if self.root == None:
-            prev_node = Node(None, None)
-            self.root = prev_node
-            for i in range(len(pattern)):
-                new_node = Node(ord(pattern[i]), prev_node)
-                prev_node.keys[ord(pattern[i])] = new_node
-                prev_node = new_node
-        else:
-            prev_node = self.root
-            for i in range(len(pattern)):
-                if prev_node.keys[ord(pattern[i])] is not None:
-                    prev_node = prev_node.keys[ord(pattern[i])]
-                else:
-                    new_node = Node(ord(pattern[i]), prev_node)
-                    prev_node.keys[ord(pattern[i])] = new_node
-                    prev_node = new_node
+    # Our add function. This is the bulk of the class
+    def add(self, text):
+    # We want to only take alpha characters in.
+    #   so if we have "Piper.", we will only store "Piper"
+        for i in range(len(text)):
+            if not ((65 <= ord(text[i]) <= 90) or (97 <= ord(text[i]) <= 122)):
+                text = text[:i] + " " + text[i+1:]
+        # Split up the input text to a list
+        text = text.split()
+        for pattern in text:
+            # If this is the first word we add, need to make sure it branches from root
+            if self.root == None:
+                prev_node = Node(False, None)
+                self.root = prev_node
+                for i in range(len(pattern)):
+                    # If we are on the last letter, we need to mark it as the end of a word
+                    if (i == (len(pattern) - 1)):
+                        new_node = Node(True, prev_node)
+                    else:
+                        new_node = Node(False, prev_node)
+                        prev_node.keys[ord(pattern[i])] = new_node
+                        prev_node = new_node
 
-    def printAllWords(self):
-        string = []
-        def recursePrint(node, string):
-            for i in range(128):
-                if node.keys[i] is not None:
-                    string += chr(node.keys[i].data)
-                    return recursePrint(node.keys[i], string)
-            return string
+            else:
+                prev_node = self.root
+                for i in range(len(pattern)):
+                    # If a node exists with our letter:
+                    if (prev_node.keys[ord(pattern[i])] is not None):
+                        # If we are on the last letter of the word being stored and the character already
+                        #   has been stored from a previous word, set the letter to mark that it is the end of the word
+                        if (i == (len(pattern) - 1)):
+                            prev_node.keys[ord(pattern[i])].word = True
+                        prev_node = prev_node.keys[ord(pattern[i])]
+                    else:
+                        # Making sure the last letter indicates it is the end of a stored word
+                        if (i == (len(pattern) - 1)):
+                            new_node = Node(True, prev_node)
+                        else:
+                            new_node = Node(False, prev_node)
+                        prev_node.keys[ord(pattern[i])] = new_node
+                        prev_node = new_node
 
-        for i in range(128):
-            if self.root.keys[i] is not None:
-                string.append(recursePrint(self.root.keys[i], chr(self.root.keys[i].data)))
+
+# This is some text code I was using
+"""
+text = "Peter Piper picked a peck of pickled peppers; A peck of pickled peppers Peter Piper picked;"
+x = Trie(text)
+text = text.split()
+for word in text:
+    print("{} is stored: {}".format(word, x.isStored(word)))
+"""    
 
 
-        return string
-
-
-x = Trie(["Hi","boo"])
-print(x.printAllWords())
-
-exit()
-print(x.isStored("Hi"))
-print(x.isStored("Hii"))
-print(x.isStored("blah"))
-print(x.root.keys[ord('H')].data)
-print(x.root.keys[ord('H')].keys[ord('i')].data)
-print(x.root.keys[ord('b')].data)
-print(x.root.keys[ord('b')].keys[ord('o')].data)
-print(x.root.keys[ord('b')].keys[ord('o')].keys[ord('o')].data)
-#print(x.root.keys[ord('b')].keys[ord('o')].keys[ord('o')].keys)
+##### ENTER CODE AFTER HERE TO TEST CLASS #####
+# initialize Trie by x = Trie(<string of words to add>)
+# COMMANDS ARE .add(<text to add>), .isStored(<text to check>)
